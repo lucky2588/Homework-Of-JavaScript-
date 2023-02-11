5d132abd4cc91605ed2a5fce2fd345b568d3f4dc
lucky2588 <duwcsthang2001@gmail.com>
2023-02-11 21:33:58 +0700

    Update main.js

--- Lesson06/lab02/main.js
+++ Lesson06/lab02/main.js
@@ -7,56 +7,55 @@ const quizes = [
     {
         id: 2,
         question: "2 + 2 = ?",
-        answers: [2, 3, 4, 5],
+        answers: [2, 4, 6, 8],
     },
     {
         id: 3,
         question: "3 + 3 = ?",
-        answers: [3, 4, 5, 6],
+        answers: [6, 9, 12, 15],
     },
 ];
 
+const btn = document.getElementById("btn");
 const quizContainer = document.querySelector(".quiz-container");
 
-function renderQuizes() {
-    let html = "";
+btn.addEventListener("click", function () {
     quizes.forEach(quiz => {
-        html += `
-            <div class="quiz-item">
-                <h3>${quiz.question}</h3>
-                <div class="quiz-answer">
-        `;
-        quiz.answers.forEach(answer => {
-            html += `
-                <div class="quiz-answer-item">
-                    <input type="radio" name="${quiz.id}">
-                    <label>${answer}</label>
-                </div>
-            `;
-        });
-        html += `
-                </div>
-            </div>
-        `;
-    });
-    quizContainer.innerHTML = html;
-}
+        const quizItem = quizContainer.querySelector(`.quiz-item[data-id="${quiz.id}"]`);
+        const quizAnswer = quizItem.querySelector(".quiz-answer");
+        const answerItems = quizAnswer.querySelectorAll(".quiz-answer-item");
 
-function randomizeAnswers() {
-    const quizItems = document.querySelectorAll(".quiz-item");
-    quizItems.forEach(quizItem => {
-        const answers = quizItem.querySelectorAll(".quiz-answer-item");
-        const randomIndex = Math.floor(Math.random() * answers.length);
-        answers.forEach((answer, index) => {
-            const input = answer.querySelector("input");
-            if (index === randomIndex) {
+        const randomAnswerIndex = Math.floor(Math.random() * answerItems.length);
+        answerItems.forEach((answerItem, index) => {
+            const input = answerItem.querySelector("input");
+            if (index === randomAnswerIndex) {
                 input.checked = true;
             } else {
                 input.checked = false;
             }
         });
     });
-}
+});
 
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", randomizeAnswers);
+let html = "";
+quizes.forEach(quiz => {
+    html += `
+        <div class="quiz-item" data-id="${quiz.id}">
+            <h3>Câu ${quiz.id} : ${quiz.question}</h3>
+            <div class="quiz-answer">
+    `;
+    quiz.answers.forEach((answer, index) => {
+        html += `
+                <div class="quiz-answer-item">
+                    <input type="radio" name="${quiz.id}">
+                    <label>${answer}</label>
+                </div>
+        `;
+    });
+    html += `
+            </div>
+        </div>
+    `;
+});
+
+quizContainer.innerHTML = html;
